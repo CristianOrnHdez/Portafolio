e95d387b7fe2c5dbdd7ed173dfb3d2053844eeca
Cristian Hernandez <cristian@MacBook-Pro-de-Cristian.local>
2022-05-21 20:04:14 -0600

    Fix controller

--- controllers/principal.js
+++ controllers/principal.js
@@ -2,10 +2,12 @@ const { request, response } = require('express')
 
 const principalGet = (req = request, res = response)=>{
     console.log('Get funcional')
+    res.send('Get funcional')
 }
 
 const principalPost = (req, res)=>{
     console.log('Post funcional')
+    res.send('Post funcional')
 }
 
 module.exports = {
